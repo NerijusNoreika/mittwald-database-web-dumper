a83eac5c86cc994d97124274f122b680d4f92e07
NerijusNoreika <Akimbas123@gmail.com>
2020-11-22 12:48:04 +0200

    Allow repeating the selection. log if user cancelled the selection.

--- dumper.js
+++ dumper.js
@@ -4,11 +4,16 @@ let databases  = iframe.contentWindow.document.querySelectorAll('div.coabfluid-t
 let databaseCount = databases[0].children.length;
 
 if (databaseCount > 0) {
-        let key = parseInt(prompt(`Found ${databaseCount} database(s). Which one you want?`));
+    do {
+        var key = parseInt(prompt(`Found ${databaseCount} database(s). Which one you want?`));
+        if (!key) break;
+        if (key > databaseCount || key < 1) {
+            alert('No such database.');
+        }
+    }
+    while (key < 1 || key > databaseCount);
+    if (key) {
         console.log(`Database ${key} will be selected...`);
-            if (key > databaseCount) {
-                alert('No such database.');
-            } else {
                 let row = databases[0].children;
                 if (row) {
                     let data = row[key -1].children;
@@ -20,7 +25,10 @@ if (databaseCount > 0) {
                 } else {
                     alert('Could not select the database');
                 }
-            }
+    } else {
+        console.log('Operation was cancelled by user.');
+    }
+        
 } else {
-    alert('No database found');
+    alert('No database found.');
 }
